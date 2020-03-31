397991ec1f59b4513a7bbc9f210275866e8b1f28
kimsappi <sappikim@gmail.com>
2020-03-31 16:25:49 +0300

    Started on accepting input for climbing weight calc

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,91 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Link} from 'react-router-dom';
 import './App.css';
+
+interface IInputDataObject {
+  name: string,
+  value: number,
+  setFunction(value: number): void,
+  unit: string
+}
+
+const Nav = () => (
+  <nav id='navbar'>
+    <ul>
+      <Link to='/'><li>Climbing weight</li></Link>
+      <Link to='/gearing'><li>Gearing</li></Link>
+    </ul>
+  </nav>
+);
+
+const RotatingWeightInput =
+  ({stateObject, eventHandler}:
+    {stateObject: IInputDataObject, eventHandler: any}) => (
+  <div>
+    {stateObject.name}
+    <input
+      type='number'
+      value={stateObject.value}
+      onChange={eventHandler}
+      id={stateObject.name.replace(' ', 'idSpacePlaceholder')}>
+    </input>
+    {stateObject.unit}
+  </div>
+);
+
+const RotatingWeightInputs = ({inputData}: {inputData: Array<IInputDataObject>}) => {
+
+  /* Single function for handling the state changes of all input values */
+  const handleDataChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const eventTargetId = event.target.id.replace('idSpacePlaceholder', ' ');
+    const dataObject = inputData.filter(object => object.name === eventTargetId)[0];
+    dataObject.setFunction(Number(event.target.value));
+  };
+
+  return (
+    <>
+      {inputData.map(entry => 
+      <RotatingWeightInput
+        stateObject={entry}
+        eventHandler={handleDataChange}
+        key={entry.name}
+      />
+      )}
+    </>
+  );
+};
+
+const RotatingWeightCalculator = () => {
+  const [rimWeight, setRimWeight] = useState(1200);
+  const [bodyWeight, setBodyWeight] = useState(70);
+  const [bikeWeight, setBikeWeight] = useState(7.5);
+  const [tyreCircumference, setTyreCircumference] = useState(2105);
+  const [speed, setSpeed] = useState(1800);
+  const [accelerationTime, setAccelerationTime] = useState(5);
+
+  const inputData: Array<IInputDataObject> = [
+    {name: 'Rim weight', value: rimWeight, setFunction: setRimWeight, unit: 'g'},
+    {name: 'Body weight', value: bodyWeight, setFunction: setBodyWeight, unit: 'kg'},
+    {name: 'Bike weight', value: bikeWeight, setFunction: setBikeWeight, unit: 'kg'},
+    {name: 'Tyre circumference', value: tyreCircumference, setFunction: setTyreCircumference, unit: 'mm'},
+    {name: 'Speed', value: speed, setFunction: setSpeed, unit: 'km/h'},
+    {name: 'Acceleration time', value: accelerationTime, setFunction: setAccelerationTime, unit: 's'}
+  ];
+
+  return (
+    <div>
+      <RotatingWeightInputs inputData={inputData} />
+    </div>
+  );
+};
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Nav />
+      <RotatingWeightCalculator />
+    </>
   );
-}
+};
 
 export default App;
