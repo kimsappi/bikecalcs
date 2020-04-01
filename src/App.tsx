078a86a9f1104798a50d70ceddb55479c60ef8e4
kimsappi <sappikim@gmail.com>
2020-04-01 08:12:05 +0300

    Initial version of rotational power calculator (there's an error somewhere)

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,23 @@ import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import './App.css';
 
-interface IInputDataObject {
+interface IVariableDataObject {
   name: string,
   value: number,
   setFunction(value: number): void,
-  unit: string
+  unit: string,
+  variableName: number // comes from enum VariableNames
+}
+
+enum VariableNames {
+  rimWeight,
+  bodyWeight,
+  bikeWeight,
+  tyreCircumference,
+  speed,
+  accelerationTime,
+  rotationalPower,
+  translationalPower
 }
 
 const Nav = () => (
@@ -20,7 +32,7 @@ const Nav = () => (
 
 const RotatingWeightInput =
   ({stateObject, eventHandler}:
-    {stateObject: IInputDataObject, eventHandler: any}) => (
+    {stateObject: IVariableDataObject, eventHandler: any}) => (
   <div>
     {stateObject.name}
     <input
@@ -33,7 +45,7 @@ const RotatingWeightInput =
   </div>
 );
 
-const RotatingWeightInputs = ({inputData}: {inputData: Array<IInputDataObject>}) => {
+const RotatingWeightInputs = ({inputData}: {inputData: Array<IVariableDataObject>}) => {
 
   /* Single function for handling the state changes of all input values */
   const handleDataChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -55,26 +67,81 @@ const RotatingWeightInputs = ({inputData}: {inputData: Array<IInputDataObject>})
   );
 };
 
+const RotatingWeightOuput = ({data}: {data: IVariableDataObject}) => {
+  return (
+    <div>
+      {data.name}
+      <input type='number' disabled value={data.value}></input>
+    </div>
+  );
+};
+
+const RotatingWeightOutputs = ({inputData, outputData}:
+  {inputData: Array<IVariableDataObject>, outputData: Array<IVariableDataObject>}) => {
+
+  /* Calculate moment of interia */
+  const momentOfInertiaCalculator = (inputData: Array<IVariableDataObject>): number => {
+    const rimWeight = inputData.filter(data => data.variableName === VariableNames.rimWeight)[0].value;
+    const tyreCircumference = inputData.filter(data => data.variableName === VariableNames.tyreCircumference)[0].value;
+    const rimRadius = tyreCircumference / 3.4; // Magic constant a bit larger than pi
+    return (rimWeight / 1000 * Math.pow(rimRadius, 2));
+  };
+
+  /* Calculate angular velocity of wheels */
+  const angularVelocityCalculator = (inputData: Array<IVariableDataObject>): number => {
+    const tyreCircumference = inputData.filter(data => data.variableName === VariableNames.tyreCircumference)[0].value;
+    const speed = inputData.filter(data => data.variableName === VariableNames.speed)[0].value;
+    return (speed / tyreCircumference / 3600 * 2 * Math.PI);
+  };
+
+  /* Calculate power required for rotational acceleration */
+  const rotationalAccelerationPowerCalculator = (inputData: Array<IVariableDataObject>) => {
+    const momentOfInertia = momentOfInertiaCalculator(inputData);
+    const angularVelocity = angularVelocityCalculator(inputData);
+    const rotationalEnergy = momentOfInertia * Math.pow(angularVelocity, 2) / 2;
+    const accelerationTime = inputData.filter(data => data.variableName === VariableNames.accelerationTime)[0].value;
+    return (rotationalEnergy / accelerationTime);
+  };
+
+  /* Set acceleration power state to result of calculation */
+  outputData[0].setFunction(rotationalAccelerationPowerCalculator(inputData));
+
+  return (
+    <div>
+      <RotatingWeightOuput data={outputData[0]} />
+      <RotatingWeightOuput data={outputData[1]} />
+    </div>
+  )
+};
+
 const RotatingWeightCalculator = () => {
   const [rimWeight, setRimWeight] = useState(1200);
   const [bodyWeight, setBodyWeight] = useState(70);
   const [bikeWeight, setBikeWeight] = useState(7.5);
   const [tyreCircumference, setTyreCircumference] = useState(2105);
-  const [speed, setSpeed] = useState(1800);
+  const [speed, setSpeed] = useState(30);
   const [accelerationTime, setAccelerationTime] = useState(5);
+  const [rotationalPower, setRotationalPower] = useState(0);
+  const [translationalPower, setTranslationalPower] = useState(0);
 
-  const inputData: Array<IInputDataObject> = [
-    {name: 'Rim weight', value: rimWeight, setFunction: setRimWeight, unit: 'g'},
-    {name: 'Body weight', value: bodyWeight, setFunction: setBodyWeight, unit: 'kg'},
-    {name: 'Bike weight', value: bikeWeight, setFunction: setBikeWeight, unit: 'kg'},
-    {name: 'Tyre circumference', value: tyreCircumference, setFunction: setTyreCircumference, unit: 'mm'},
-    {name: 'Speed', value: speed, setFunction: setSpeed, unit: 'km/h'},
-    {name: 'Acceleration time', value: accelerationTime, setFunction: setAccelerationTime, unit: 's'}
+  const inputData: Array<IVariableDataObject> = [
+    {name: 'Rim weight', value: rimWeight, setFunction: setRimWeight, unit: 'g', variableName: VariableNames.rimWeight},
+    {name: 'Body weight', value: bodyWeight, setFunction: setBodyWeight, unit: 'kg', variableName: VariableNames.bodyWeight},
+    {name: 'Bike weight', value: bikeWeight, setFunction: setBikeWeight, unit: 'kg', variableName: VariableNames.bikeWeight},
+    {name: 'Tyre circumference', value: tyreCircumference, setFunction: setTyreCircumference, unit: 'mm', variableName: VariableNames.tyreCircumference},
+    {name: 'Speed', value: speed, setFunction: setSpeed, unit: 'km/h', variableName: VariableNames.speed},
+    {name: 'Acceleration time', value: accelerationTime, setFunction: setAccelerationTime, unit: 's', variableName: VariableNames.accelerationTime}
+  ];
+
+  const outputData: Array<IVariableDataObject> = [
+    {name: 'Power required to accelerate wheels', value: rotationalPower, setFunction: setRotationalPower, unit: 'W', variableName: VariableNames.rotationalPower},
+    {name: 'Power required to accelerate the bike and yourself', value: translationalPower, setFunction: setTranslationalPower, unit: 'W', variableName: VariableNames.translationalPower}
   ];
 
   return (
     <div>
       <RotatingWeightInputs inputData={inputData} />
+      <RotatingWeightOutputs inputData={inputData} outputData={outputData} />
     </div>
   );
 };
